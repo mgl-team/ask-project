051f7eefbbaf212ddc62c0a9aab3be1f7f9d22b7
amjil <amjil@protonmail.com>
2021-12-15 20:08:32 +0800

    routes

--- src/clj/app/routes/services.clj
+++ src/clj/app/routes/services.clj
@@ -14,61 +14,71 @@
     [clojure.java.io :as io]
     [spec-tools.data-spec :as ds]
 
-    [app.services.user.login :as login-service]))
+    [app.services.user.login :as login-service]
+    [app.routes.question :as question-route]
+    [app.routes.answer :as answer-route]
+    [app.routes.topic :as topic-route]))
 
 (defn service-routes []
-  ["/api"
-   {:coercion spec-coercion/coercion
-    :muuntaja formats/instance
-    :swagger {:id ::api}
-    :middleware [;; query-params & form-params
-                 parameters/parameters-middleware
-                 ;; content-negotiation
-                 muuntaja/format-negotiate-middleware
-                 ;; encoding response body
-                 muuntaja/format-response-middleware
-                 ;; exception handling
-                 exception/exception-middleware
-                 ; coercion/coerce-exceptions-middleware
-                 ;; decoding request body
-                 muuntaja/format-request-middleware
-                 ;; coercing response bodys
-                 coercion/coerce-response-middleware
-                 ;; coercing request parameters
-                 coercion/coerce-request-middleware
-                 ;; multipart
-                 multipart/multipart-middleware]}
+  (into []
+        (concat
+         ["/api"
+          {:coercion spec-coercion/coercion
+           :muuntaja formats/instance
+           :swagger {:id ::api}
+           :middleware [;; query-params & form-params
+                        parameters/parameters-middleware
+                        ;; content-negotiation
+                        muuntaja/format-negotiate-middleware
+                        ;; encoding response body
+                        muuntaja/format-response-middleware
+                        ;; exception handling
+                        exception/exception-middleware
+                        ; coercion/coerce-exceptions-middleware
+                        ;; decoding request body
+                        muuntaja/format-request-middleware
+                        ;; coercing response bodys
+                        coercion/coerce-response-middleware
+                        ;; coercing request parameters
+                        coercion/coerce-request-middleware
+                        ;; multipart
+                        multipart/multipart-middleware]}
 
-   ;; swagger documentation
-   ["" {:no-doc true
-        :swagger {:info {:title "my-api"
-                         :description "https://cljdoc.org/d/metosin/reitit"}}}
+          ;; swagger documentation
+          ["" {:no-doc true
+               :swagger {:info {:title "my-api"
+                                :description "https://cljdoc.org/d/metosin/reitit"}}}
 
-    ["/swagger.json"
-     {:get (swagger/create-swagger-handler)}]
+           ["/swagger.json"
+            {:get (swagger/create-swagger-handler)}]
 
-    ["/api-docs/*"
-     {:get (swagger-ui/create-swagger-ui-handler
-             {:url "/api/swagger.json"
-              :config {:validator-url nil}})}]]
+           ["/api-docs/*"
+            {:get (swagger-ui/create-swagger-ui-handler
+                    {:url "/api/swagger.json"
+                     :config {:validator-url nil}})}]]
 
-   ["/ping"
-    {:get (constantly (ok {:message "pong"}))}]
+          ["/ping"
+           {:get (constantly (ok {:message "pong"}))}]
 
 
-   ["/user"
-    {:swagger {:tags ["user"]}}
-    ["/login"
-     {:post {:summary "login."
-             :parameters {:body {(ds/opt :username) string? (ds/opt :password) string?
-                                 (ds/opt :code) string? :token string?
-                                 (ds/opt :mobile) string?
-                                 (ds/opt :email) string?}}
-             :responses {200 {:body {:code int? :msg string?, (ds/opt :errors) any?
-                                                            , (ds/opt :token) any?}}}
-             :handler (fn [{{:keys [body]} :parameters headers :headers addr :remote-addr}]
-                        {:status 200 :body
-                         (login-service/login body headers addr)})}}]]])
+          ["/user"
+           {:swagger {:tags ["user"]}}
+           ["/login"
+            {:post {:summary "login."
+                    :parameters {:body {(ds/opt :username) string? (ds/opt :password) string?
+                                        (ds/opt :code) string? :token string?
+                                        (ds/opt :mobile) string?
+                                        (ds/opt :email) string?}}
+                    :responses {200 {:body {:code int? :msg string?, (ds/opt :errors) any?
+                                                                   , (ds/opt :token) any?}}}
+                    :handler (fn [{{:keys [body]} :parameters headers :headers addr :remote-addr}]
+                               {:status 200 :body
+                                (login-service/login body headers addr)})}}]]]
+
+         ;; other routes
+         question-route/route
+         answer-route/route
+         topic-route/route)))
 
 
    ; ["/math"
